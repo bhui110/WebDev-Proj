12daaedcad87a4591eaaeb1a6d733e310c3e8002
tina.leunggg <tinaisworking@gmail.com>
2025-03-27 17:31:03 -0700

    box layout

--- game.js
+++ game.js
@@ -1,10 +1,53 @@
-let boxes = document.getElementsByClassName("box");
-let container = document.getElementById("box-container");
+// create random order
+let random_order = generate_random();
+console.log(random_order);
 
-let containerWidth = container.offsetWidth;
-let position = container.getBoundingClientRect();
-let leftBonud = container.getBoundingClientRect().left;
-let rightBound = container.getBoundingClientRect().left + containerWidth;
+// create innerHTML for random order
+let boxes_element = '<div class="box" id="1">1</div>';
+for (let i = 0; i < random_order.length; i++){
+    boxes_element += `<div class="box" id="${random_order[i]}">${random_order[i]}</div>`;
+}
+boxes_element += '<div class="box" id="10">10</div>'
+
+let container = document.getElementById("box-container");
+container.innerHTML = boxes_element;
+
+
+// changing color one by one according to the list
+let boxes = document.querySelectorAll(".box");
+let colors = ["#000000", "#101010", "#202020", "#303030","#404040", "#505050", "#606060", "#707070", "#808080", "#ffffff"]; 
+// colors should be a list of color in correct order, the first and the last can be generated randomly, 
+// colors in between are automatically calculated
+
+for (let i = 0; i < boxes.length; i++){
+    let id = (i+1).toString();
+    document.getElementById(id).style.backgroundColor = colors[i];
+}
+
+// positioning the boxes : set px in absolute positioning
+for (let i = 0; i < boxes.length; i++){
+    boxes[i].style.left = `${85*i}px`;
+}
+
+
+// loop through the element in new and change the position
+
+
+
+
+// drag and drop
+
+// add event listener to div box number 2 - 9
+
+// if dragging, detect position of drag and move the element according to position
+    // break down position of container into ten portions, 
+    // if the element is dragger to next box
+        //create a new boxes element and set innerHTML to new boxes_element
+
+// let containerWidth = container.offsetWidth;
+// let position = container.getBoundingClientRect();
+// let leftBonud = container.getBoundingClientRect().left;
+// let rightBound = container.getBoundingClientRect().left + containerWidth;
 
 
 // console.log(containerWidth);
@@ -12,36 +55,7 @@ let rightBound = container.getBoundingClientRect().left + containerWidth;
 // console.log(rightBound);
 
 
-
-// create random order
-let random_order = [2,3,4,5,6,7,8,9];
-let colors = ["#000000", "101010", "#202020", "#303030","#404040", "#505050", "#606060", "#707070", "#808080", "#909090"];
-let boxes_element = '';
-for (let i = 0; i < random_order.length; i++){
-    let n = Math.floor(Math.random()*8);
-    let a = random_order[i];
-    let b = random_order[n];
-    random_order[n] = a;
-    random_order[i] = b;
-}
-
-// correct ans is always 1,2,3,4,5,6,7,8,9,10, assign the order of color in acending color
-let correct_ans = '';
-for (let n = 1; n < 11; n++) {
-    correct_ans += `<div class="box" id="${n}">${n}</div>`;
-}
-
-
-boxes_element = '<div class="box" id="1">1</div>'
-for (let i = 0; i < random_order.length; i++){
-    boxes_element += `<div class="box" id="${random_order[i]}">${random_order[i]}</div>`;
-}
-boxes_element += '<div class="box" id="10">10</div>'
-
-
-container.innerHTML = boxes_element;
-
-
+// sample code from stackoverflow
 let dragging = false;
 let box = document.getElementById("2")
 box.addEventListener('mousedown', function(evt){
@@ -68,10 +82,23 @@ if(dragging){
 });
 
 
-// add event listener to div box number 2 - 9
 
-// if dragging, detect position of drag and move the element according to position
-    // break down position of container into ten portions, 
-    // if the element is dragger to next box
-        //create a new boxes element and set innerHTML to new boxes_element
-// 
+// ------------------------------------------------
+
+// correct ans is always 1,2,3,4,5,6,7,8,9,10, assign the order of color in acending color
+let correct_ans = '';
+for (let n = 1; n < 11; n++) {
+    correct_ans += `<div class="box" id="${n}">${n}</div>`;
+}
+
+function generate_random(){
+    let order = [2,3,4,5,6,7,8,9];
+    for (let i = 0; i < order.length; i++){
+        let n = Math.floor(Math.random()*8);
+        let a = order[i];
+        let b = order[n];
+        order[n] = a;
+        order[i] = b;
+    }
+    return order;
+}
