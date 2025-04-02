02815ac0155e0b178d4360cc10cdf9a6c84bbf5e
tina.leunggg <tinaisworking@gmail.com>
2025-04-02 13:53:51 -0700

    fix bug: drag and drop

--- game.js
+++ game.js
@@ -18,28 +18,51 @@ let mouseStart;
 let containerLeft = container.getBoundingClientRect().left;
 let leftBoundary = containerLeft + 85;
 let rightBoundary = containerLeft + 85*8;
-let currentOrder = random_order; // innerHTML string
+let currentOrder = random_order;
 let scoreBox = document.getElementById("score");
-let log = document.getElementById("log");
+let topBox = document.querySelector(".top");
+// let log = document.getElementById("log");
 let startIndex;
-
+let answer = [2,3,4,5,6,7,8,9]  // correct ans is always 1,2,3,4,5,6,7,8,9,10, assign color in acending color
 
 setBoxHandler()
 setDragHandler()
 setDropHandler()
 
-let button = document.getElementsByTagName("button");
+let button = document.getElementById("checkAns");
 button.onclick = displayResult;
 
-// ----------------------------------- check answer, display result ------------------------------
-
-// correct ans is always 1,2,3,4,5,6,7,8,9,10, assign the order of color in acending color
-
-
 function displayResult(){
-    
-}
 
+    let right = 0;
+    for (let i = 0; i < answer.length ; i ++){
+    if (answer[i] === currentOrder[i]){
+        right++;
+    } 
+    }
+    let score = right/8 * 100;
+    scoreBox.innerHTML = "Accuracy: " + score + "%";
+
+    if (score == 100){
+        topBox.innerHTML = "<p>Perfect!</p>";
+        topBox.classList.add("perfect");
+        // topBox.style.color = 'white';
+    } else if (score > 40){
+        topBox.innerHTML = "<p>Good!</p>";
+        topBox.classList.add("good");
+    } else {
+        topBox.innerHTML = "<p>Try again!</p>";
+        topBox.classList.add("tryAgain");
+        topBox.style.color = 'white';
+    }
+
+
+
+
+
+    button.innerHTML = "restart";
+    button.onclick = reload => {window.location.reload();}
+}
 
 function setBoxHandler(){
     for (let i = 1; i < (boxes.length -1) ; i++){ //only apply to index 1-8
@@ -66,7 +89,7 @@ function setDragHandler(){
             boxPosition = mousePosition - containerLeft - offsetInBox;
         } else if (mousePosition < leftBoundary + offsetInBox){
             boxPosition = leftBoundary - containerLeft ;
-        } else if (mousePosition <= rightBoundary + offsetInBox) {   
+        } else if (mousePosition > rightBoundary + offsetInBox) {   
             boxPosition = rightBoundary - containerLeft ; 
         }
     
@@ -76,23 +99,31 @@ function setDragHandler(){
 }
 
 function setDropHandler(){
-        
+
     window.addEventListener('mouseup', function(event){
-        dragging = false;
-        boxSelected.style.zIndex = "1"; // ?? not working
 
+        if (event.target === boxSelected){
 
-        let i = Math.floor((event.pageX - containerLeft)/85) -1; // new index
-        console.log(i);
-        if (event.pageX > mouseStart + 40 || event.pageX < mouseStart - 40){ // moving right
-            reArangeOrder(i, 1);
+            dragging = false;
+
+            let i = Math.floor((event.pageX - containerLeft)/85) -1; 
+            if (i < 0){
+                i = 0;
+            } else if (i > 7){
+                i = 7;
+            }
+            if (event.pageX > mouseStart + 40 || event.pageX < mouseStart - 40){
+                reArangeOrder(i, 1);
+            }
+
+            changeHTMLBox(currentOrder);
+            boxes = document.querySelectorAll(".box"); // box element is updated, re-do all the steps to set up boxes
+            coloringBox();
+            positionBox();
+            setBoxHandler();
         }
+        // fixed bug: when click on confirm button, windows event is triggered, boxes are re-arranged
 
-        changeHTMLBox(currentOrder);
-        boxes = document.querySelectorAll(".box"); // box element is updated, re-do all the steps to set up boxes
-        coloringBox();
-        positionBox();
-        setBoxHandler();
     });
 }
 
@@ -114,8 +145,7 @@ function reArangeOrder(i){
     let s = startIndex;
     let moving = currentOrder[s]
     currentOrder.splice(s, 1);
-    currentOrder.splice(i, 0, moving);    
-    log.innerHTML = currentOrder;
+    currentOrder.splice(i, 0, moving);
 }
 
 function changeHTMLBox(order){
@@ -124,7 +154,7 @@ function changeHTMLBox(order){
         boxes_element += `<div class="box" id="${order[i]}">${order[i]}</div>`;
     }
     boxes_element += '<div class="box" id="10">10</div>'
-    container.innerHTML = boxes_element;   // global variable
+    container.innerHTML = boxes_element;
 }
 
 function coloringBox(){
@@ -138,4 +168,12 @@ function positionBox(){
     for (let i = 0; i < boxes.length; i++){
         boxes[i].style.left = `${85*i}px`;
     }
+}
+
+function generateColor(){
+    let color = [];
+    let r;
+    let g;
+    let b;
+    
 }
